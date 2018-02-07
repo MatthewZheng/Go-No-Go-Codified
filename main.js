5b5d1a942123242a8969f5ba006027e6b5f5ab6a
MatthewZheng <mattlzheng@gmail.com>
2018-02-07 15:34:15 -0800

    Added basic logic framework

---
+++ main.js
@@ -0,0 +1,18 @@
+var math = require("mathjs");
+
+var cosSim = (function Mange(){
+    var modules = {};
+    
+    function countStr(someS){
+        return(someS.split.forEach(v => this[v] = (this[v] || 0 +1)));
+    };
+
+    function makeMatrix(str1, str2){
+        var count1 = countStr(str1), count2 = countStr(str2);
+        var join1 = new Set(count1.keys()), join2 = new Set(count2.keys());
+    
+        
+    
+    };
+
+})();
